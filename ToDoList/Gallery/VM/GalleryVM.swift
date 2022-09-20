20abb3b8773a610badfc866bc5f6c5621868f528
Berkay <berkayyalcn1@gmail.com>
2022-09-20 12:22:37 +0300

    Gallery Page Done

--- ToDoList/Gallery/VM/GalleryVM.swift
+++ ToDoList/Gallery/VM/GalleryVM.swift
@@ -12,7 +12,6 @@ protocol GallertyVMToViewProtocol: AnyObject {
     func didCellItemFetch(_ items: [PhotoCellViewModel])
 }
 
-
 class GalleryVM{
     
     private let model = GalleryModel()
@@ -27,10 +26,22 @@ class GalleryVM{
     }
 }
 
+private extension GalleryVM {
+    
+    @discardableResult
+    func makeViewBasedModel(_ photos: [Photos]) -> [PhotoCellViewModel] {
+        return photos.map { .init(url: $0.url)}
+    }
+}
 
 extension GalleryVM: GalleryModelToViewModelProtocol {
     
     func didDataFetchProccesFinish(_ isSuccess: Bool) {
         
+        if isSuccess {
+            let photos = model.photos.filter { $0.id < 101 }
+            let cellModels = makeViewBasedModel(photos)
+            viewDelegate?.didCellItemFetch(cellModels)
+        }
     }
 }
